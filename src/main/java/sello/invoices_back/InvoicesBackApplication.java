2d4b9ccb748dd20fb8a44072a5b0106139fc9c01
maxenceprsll <maxence.persello@icloud.com>
2025-04-13 23:57:27 +0200

    init repo

---
+++ src/main/java/sello/invoices_back/InvoicesBackApplication.java
@@ -0,0 +1,13 @@
+package sello.invoices_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesBackApplication.class, args);
+	}
+
+}
